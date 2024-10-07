9b4855d62e7a1c78f60463c666e9f16ca5abffb3
Durai261 <durai2698@gmail.com>
2024-10-07 10:15:17 +0530

    updated Container

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -3,6 +3,8 @@ set -e
 
 # Pull the Docker image from Docker Hub
 echo
+docker push durai261/simple-python-flask-app1
 
 # Run the Docker image as a container
+docker run -d -p 5000:5000 durai261/simple-python-flask-app1
 echo
